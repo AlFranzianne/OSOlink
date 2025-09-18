6904474ec9641c34d7da74e6c8563485543271e2
krispypcl <krispypcl@gmail.com>
2025-09-19 01:46:25 +0800

    Updated project.index section class

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,7 +2,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
-            <section>
+            <section class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <header class="flex items-center justify-between">
                     <div>
                         <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
